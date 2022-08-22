88104578ddb59573597676dec8828819a3e78331
Sanjay Dutta <sanjay.aug.02.1998@gmail.com>
2022-08-22 13:07:35 +0530

    First commit

---
+++ .nuxt/dist/client/9b30833.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{252:function(n,e,l){"use strict";l.r(e);var t={name:"IndexPage"},o=l(56),component=Object(o.a)(t,(function(){return(0,this._self._c)("div",[this._v("Hello World 2022")])}),[],!1,null,null,null);e.default=component.exports}}]);
